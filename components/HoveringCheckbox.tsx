837dd7dee3a0a40a9b0dd07961f18b964bfe5b38
Sparsh Singh <sparshsingh7586@gmail.com>
2024-10-13 22:35:44 +0530

    Initial Commit

---
+++ components/HoveringCheckbox.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { motion } from "framer-motion";
+
+const HoveringCheckbox = ({
+  positions,
+  children,
+}: {
+  positions: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <motion.div
+      animate={{
+        y: [0, 25],
+      }}
+      transition={{
+        duration: 3,
+        repeat: Infinity,
+        repeatType: "mirror",
+        ease: "easeInOut",
+      }}
+      style={{
+        willChange: "transition",
+      }}
+      className={
+        "opacity-50 absolute text-3xl flex justify-center items-center gap-6 px-7 py-3 border border-white/50 bg-transparent backdrop-blur rounded-2xl " +
+        positions
+      }
+    >
+      {children}
+    </motion.div>
+  );
+};
+export default HoveringCheckbox;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      // fontFamily: {
+      //   work: ["var(--font-work-sans)"],
+      // },
     },
   },
   plugins: [],
