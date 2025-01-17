fa978f260e8ee3a26ede1d6a4de72a75a329b6e4
bigsparsh <sparshsingh7586@gmail.com>
2025-01-17 22:40:01 +0530

    Made the range output field responsive

--- components/RatingOutputField.tsx
+++ components/RatingOutputField.tsx
@@ -18,36 +18,41 @@ const RatingOutputField = ({
   return (
     <div className="flex flex-col rounded-3xl overflow-clip border border-neutral-700 bg-neutral-700">
       <h1
-        className={`px-3 md:px-7 xl:px-10 py-2 font-medium md:font-semibold text-xl md:text-2xl`}
+        className={`px-5 md:px-7 xl:px-10 pt-4 font-normal md:font-semibold text-xl md:text-2xl`}
       >
         {group_name}
       </h1>
       <div
-        className="work grid place-items-center p-5"
+        className="work flex flex-col gap-1 md:gap-2 p-5 overflow-x-auto"
         style={{
           gridTemplateColumns: `repeat(${ratingLabels.length + 1}, minmax(0, 1fr))`,
         }}
       >
-        <div></div>
-        {ratingLabels.map((label) => {
-          return (
-            <div
-              key={label.index}
-              className="font-semibold w-full h-full bg-neutral-600 text-center p-2 rounded-3xl"
-            >
-              {label.label}
-            </div>
-          );
-        })}
+        <div className="flex md:gap-2 gap-1">
+          <div className="max-w-[200px] p-2 min-w-[150px] h-full w-full"></div>
+          {ratingLabels.map((label) => {
+            return (
+              <div
+                key={label.index}
+                className="font-semibold text-sm md:text-base max-w-[200px] min-w-[150px] w-full h-full bg-neutral-600 text-center p-2 rounded-xl"
+              >
+                {label.label}
+              </div>
+            );
+          })}
+        </div>
         {ratingHeadings.map((heading) => {
           return (
-            <>
-              <div className="font-semibold w-full h-full bg-neutral-600 text-center p-2 rounded-3xl">
+            <div className="flex md:gap-2 gap-1" key={heading.rating_group_id}>
+              <h1 className="font-semibold text-sm md:text-base w-full h-full bg-neutral-600 text-center p-2 rounded-xl max-w-[200px] min-w-[150px]">
                 {heading.title}
-              </div>
+              </h1>
               {ratingLabels.map((label) => {
                 return (
-                  <div key={label.ratingLabel_id}>
+                  <div
+                    key={label.ratingLabel_id}
+                    className="w-full max-w-[200px] min-w-[150px] grid place-items-center"
+                  >
                     <input
                       type="radio"
                       name={heading.field_id}
@@ -63,7 +68,7 @@ const RatingOutputField = ({
                   </div>
                 );
               })}
-            </>
+            </div>
           );
         })}
       </div>
